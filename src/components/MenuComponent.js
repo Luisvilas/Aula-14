01f0c3e5d31a6687affde6e1f590d91f797af2d5
luis <luisvboas@outlook.com>
2024-12-05 19:42:01 -0300

    Aula 14

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import DishDetail from './DishdetailComponent';
+
+const Menu = (props) => {
+    const [selectedDish, setSelectedDish] = useState(null);
+ 
+    const onDishSelect = (dish) => {
+        setSelectedDish(dish);
+    };
+
+    const menu = props.dishes.map((dish) => {
+        return (
+            <div className="col-12 col-md-5 m-1" key={dish.id}>
+                <Card onClick={() => onDishSelect(dish)}>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Card>
+            </div>
+        );
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                {menu}
+            </div>
+            <DishDetail dish={selectedDish} />
+        </div>
+    );
+};
+
+export default Menu;
